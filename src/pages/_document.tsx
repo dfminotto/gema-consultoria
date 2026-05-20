d062ec45436e06b6c94abd2c5c375627f9c8cae1
Djone <dfminoto@gmail.com>
2026-05-20 08:51:38 +0100

    Fix charset and ESLint config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        <meta charSet="UTF-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
