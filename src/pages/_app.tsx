4517c2b9719bf3a48f499903de95aafe93c06f3f
Djone <dfminoto@gmail.com>
2026-05-20 08:53:38 +0100

    Fix charSet in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
